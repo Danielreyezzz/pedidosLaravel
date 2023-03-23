a9ef53ea69c120f0a02caf67a1b2332d43878735
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2023-03-23 11:58:08 +0100

    seeder ok

---
+++ pedidosLaravel/database/pedidos.sql
@@ -0,0 +1,19 @@
+INSERT INTO orders (provincia, localidad, direccion, codigo_postal, entregado, comentario, hora_entrega, bultos, peso) 
+VALUES 
+  ('Barcelona', 'Sant Cugat del Vallès', 'Calle Mayor 123', '08172', true, 'Pedido urgente', '2023-03-23 10:30:00', 2, 4.5),
+  ('Madrid', 'Alcalá de Henares', 'Plaza Cervantes 1', '28801', false, 'Pedido normal', '2023-03-23 12:00:00', 1, 1.2),
+  ('Valencia', 'Torrent', 'Avenida Principal 45', '46900', true, 'Pedido importante', '2023-03-23 14:15:00', 3, 10.0),
+  ('Sevilla', 'Dos Hermanas', 'Calle Sevilla 18', '41700', false, 'Pedido con descuento', '2023-03-23 16:00:00', 1, 0.8),
+    ('Madrid', 'Madrid', 'Calle Gran Vía, 1', '28013', true, 'Sin comentarios', '2023-03-23 14:30:00', 3, 10.50),
+  ('Barcelona', 'Barcelona', 'Calle Diagonal, 123', '08001', false, 'Entregar en la puerta principal', '2023-03-22 12:00:00', 2, 8.20),
+  ('Valencia', 'Valencia', 'Avenida del Puerto, 50', '46023', true, 'Recoger en tienda', '2023-03-22 10:00:00', 1, 5.00),
+  ('Sevilla', 'Sevilla', 'Plaza de España, s/n', '41004', false, 'Entregar en portería', '2023-03-21 15:00:00', 4, 12.80),
+  ('Asturias', 'Oviedo', 'Calle Uría, 15', '33003', true, 'Dejar en recepción', '2023-03-20 11:30:00', 2, 6.70),
+  ('Alicante', 'Alicante', 'Calle San Francisco, 10', '03001', false, 'Contactar antes de entregar', '2023-03-19 16:45:00', 3, 11.00),
+  ('Cádiz', 'Cádiz', 'Calle San Juan de Dios, 5', '11005', true, 'Dejar en el buzón', '2023-03-18 09:00:00', 1, 3.50);
+  
+  
+  
+
+ 
+
