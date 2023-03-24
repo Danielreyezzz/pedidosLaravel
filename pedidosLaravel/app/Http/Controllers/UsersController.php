295e18e2a93e105288feb5ed8a282177d49250ce
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2023-03-24 12:46:46 +0100

    validation

--- pedidosLaravel/app/Http/Controllers/UsersController.php
+++ pedidosLaravel/app/Http/Controllers/UsersController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    
+  public function datos (Request $request){
+
+    $request->validate([
+
+        'email'=>'required|string|email|min:0|max:255\regex:/(.*)@(.*)\.(es|com|org)/i',
+        'password'=>'password|min:4',
+        
+    ]);
+  }  
 }
