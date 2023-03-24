5977eb9f4c0d06ead69b3a9db432208ec997c1cf
Danielreyezzz <dangioparefape@gmail.com>
2023-03-24 12:52:26 +0100

    Panel usuario

--- pedidosLaravel/app/Http/Controllers/UsersController.php
+++ pedidosLaravel/app/Http/Controllers/UsersController.php
@@ -7,21 +7,18 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-<<<<<<< HEAD
     public function getData()
     {
         $users = User::all();
         return view('panelusuario', @compact('users'));
     }
-=======
   public function datos (Request $request){
 
     $request->validate([
 
         'email'=>'required|string|email|min:0|max:255\regex:/(.*)@(.*)\.(es|com|org)/i',
         'password'=>'password|min:4',
-        
+
     ]);
-  }  
->>>>>>> 295e18e2a93e105288feb5ed8a282177d49250ce
+  }
 }
